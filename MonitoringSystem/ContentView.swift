8815c2c986fbd44c1022b2155e3fb8ebaafd1f74
EitoNiiyama <niiyama8.ltlyl87@gmail.com>
2025-01-27 15:40:04 +0900

    Initial Commit

---
+++ MonitoringSystem/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MonitoringSystem
+//
+//  Created by 新井山詠斗 on 2025/01/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MonitoringSystem/MonitoringSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitoringSystemApp.swift
+//  MonitoringSystem
+//
+//  Created by 新井山詠斗 on 2025/01/27.
+//
+
+import SwiftUI
+
+@main
+struct MonitoringSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
